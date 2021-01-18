a8c0d021a69dff14921f1e1bd1b817d8a422bb6e
MWall <mjwall@student.fullsail.edu>
2021-01-17 20:11:46 -0500

    working on html pages

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var fs = require("fs");
 var http = require("http");
+var path = require('path');
+var url = require('url');
 
 http.createServer(function(req,res){
 
-    fs.readFile("file.html",function(err,data){
+    fs.readFile("file",function(err,data){
+
+        var parsed = url.parse(req.url);
+        var filename = path.parse(parsed.pathname);
+
+        console.log(filename.name);
+        filen = filename.name;
+
+        if(filename.name == ""){
+            filen = "index"
+        }else{
+            filen = filename.name;
+        }
 
         res.writeHead(200);
         res.end(data);
