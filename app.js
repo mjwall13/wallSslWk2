b46f6199c088a59819687d263ef131bab8a6be3f
MWall <mjwall@student.fullsail.edu>
2021-02-14 17:38:22 -0500

    tried to get navigation to work

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-var fs = require("fs");
-var http = require("http");
-var path = require("path");
-var url = require("url");
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const url = require('url');
 
 http.createServer(function(req,res){
 
-    // var parsed = url.parse(req.url);
-    // var filename = path.parse(parsed.pathname);
-    // console.log(filename);
+    var parsed = req.url;
+    var pathname = parsed.hash;
 
-    // if(filename.name == ""){
-    //     filen = "index";
-    // }else{
-    //     filen = filename.name;
-    // }
-
-    fs.readFile("index.html",function(err,data){
-        res.writeHead(200);
-        res.end(data);
-    })
-
+    if(pathname === "about"){
+        fs.readFile("about.html",function(err,data){
+            res.writeHead(200);
+            res.end(data);
+        })
+        }
+    else if(pathname === "contact") {
+        fs.readFile("contact.html",function(err,data){
+            res.writeHead(200);
+            res.end(data);
+        })
+    
+    }else{
+        fs.readFile("index.html",function(err,data){
+            res.writeHead(200);
+            res.end(data);
+        })
+    }
 }).listen("8080")
