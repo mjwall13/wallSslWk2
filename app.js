a511fb02476654002aba5cd2871bc49d9776e2f1
MWall <mjwall@student.fullsail.edu>
2021-02-21 16:17:33 -0500

    started assignment 4

--- app.js
+++ app.js
@@ -3,27 +3,70 @@ const http = require("http");
 const path = require("path");
 const url = require('url');
 
-http.createServer(function(req,res){
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-    var parsed = req.url;
-    var pathname = parsed.hash;
+let ejs = require("ejs")
+const router = express.Router();
+var app = express();
+app.set("view enjine", "ejs");
+app.engine("ejs", require("ejs")._express);
 
-    if(pathname === "about"){
-        fs.readFile("about.html",function(err,data){
-            res.writeHead(200);
-            res.end(data);
-        })
-        }
-    else if(pathname === "contact") {
-        fs.readFile("contact.html",function(err,data){
-            res.writeHead(200);
-            res.end(data);
-        })
-    
-    }else{
-        fs.readFile("index.html",function(err,data){
-            res.writeHead(200);
-            res.end(data);
-        })
+router.get("/",function(req,res){
+    res.render("index", {pagename: "Home"}); //views/index.ejs
+})
+router.get("/about",function(req,res){
+    res.render("about", {pagename: "About"}); //views/about.ejs
+})
+
+router.post("/login", function(req,res){
+    var errors = [];
+    var regex = /[^\s@]+@[^\s@]+\.[^\s@]+/;
+
+    if(req.body.email ==""){
+        errors.push("Email is required.")
     }
-}).listen("8080")
+    if(req.body.password ==""){
+        errors.push("Password is required.")
+    }
+    if (regex.test(req.body.email)) {
+        res.redirect("/");
+    } 
+
+})
+
+app.use(express.static("public"));
+app.use("/",router);
+var server = app.listen("8080");
+
+
+
+
+
+// http.createServer(function(req,res){
+
+//     var parsed = req.url;
+//     var pathname = parsed.hash;
+
+//     if(pathname = "about"){
+//         fs.readFile("contact.html",function(err,data){
+//             res.writeHead(200);
+//             res.end(data);
+//         })
+//         }
+//     else if(pathname = "contact") {
+//         fs.readFile("contact.html",function(err,data){
+//             res.writeHead(200);
+//             res.end(data);
+//         })
+    
+//     }else{
+//         fs.readFile("index.html",function(err,data){
+//             res.writeHead(200);
+//             res.end(data);
+//         })
+//     }
+// }).listen("8080")
---
+++ assignment4.js
@@ -0,0 +1,48 @@
+// var http = require('htt');
+var http = require('http');
+
+// var myname = functon
+function myname(){
+  console.log("Here is my IP address");
+}
+
+function callHttpbin() {
+//   let promise = new Promise((resolve, reject) => {
+    http.get(
+     'http://httpbin.org/ip',
+     function(response) {
+      var str="";
+      response.setEncoding('utf8');
+      response.on('data', function(data){
+      str += data;
+     });
+     response.on('end', function() {
+      var result = JSON.parse(str);
+
+      myips = result.origin;
+      resolve()
+     });
+     }
+    );
+// });
+
+// let result = await promise;
+result;
+};
+async function executeAsyncTask(){
+  const valueA = callHttpbin();
+  const valueB = myname();
+
+  let promise = new Promise((resolve, reject) => {
+    fs.readFile('myfile.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+      resolve(data)
+    });
+  });
+  
+  let result = await promise;
+
+
+  console.log(valueB+" "+valueA)
+// Output Here is my IP address 149.24.160.1, 149.24.160.1
+}
